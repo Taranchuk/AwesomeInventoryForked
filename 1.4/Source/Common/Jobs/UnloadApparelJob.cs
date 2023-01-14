4442082af673866bf68708fa0f88d80972d7c0c6
xandrmoro <xandrmoro@gmail.com>
2023-01-14 18:09:12 +0100

    Project structure redone

---
+++ 1.4/Source/Common/Jobs/UnloadApparelJob.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="UnloadApparelJob.cs" company="Zizhen Li">
+// Copyright (c) 2019 - 2020 Zizhen Li. All rights reserved.
+// Licensed under the LGPL-3.0-only license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+
+using Verse;
+using Verse.AI;
+
+namespace AwesomeInventory.Jobs
+{
+    /// <summary>
+    /// A job to unload extra apparels on pawn.
+    /// </summary>
+    public class UnloadApparelJob : Job
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnloadApparelJob"/> class.
+        /// </summary>
+        public UnloadApparelJob()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnloadApparelJob"/> class.
+        /// </summary>
+        /// <param name="localTargetInfo"> Apparel to unload. </param>
+        public UnloadApparelJob(LocalTargetInfo localTargetInfo)
+            : base(AwesomeInventory_JobDefOf.AwesomeInventory_Unload, localTargetInfo)
+        {
+        }
+    }
+}
---
+++ 1.4/Source/Common/Utilities/ThirdPartyMods/PickUpAndHaulUtility.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="PickUpAndHaulUtility.cs" company="Zizhen Li">
+// Copyright (c) 2019 - 2020 Zizhen Li. All rights reserved.
+// Licensed under the LGPL-3.0-only license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Verse;
+
+namespace AwesomeInventory
+{
+    /// <summary>
+    /// Utility support for Pickup and haul.
+    /// </summary>
+    public static class PickUpAndHaulUtility
+    {
+        [SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "Lower case is required.")]
+        private static string _packageID = "Mehni.PickUpAndHaul".ToLowerInvariant();
+
+        static PickUpAndHaulUtility()
+        {
+            IsActive = LoadedModManager.RunningModsListForReading.Any(m => m.PackageId == _packageID);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether pickup and Haul is actived in this save.
+        /// </summary>
+        public static bool IsActive { get; private set; }
+    }
+}
